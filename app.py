2bfa48b7763fd207168621050b3e7316747525fb
Juan Castro <juan.manuel.castro.arias@gmail.com>
2022-09-08 10:30:13 +0200

    clean version

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import pandas as pd
-#from datetime import datetime
-#from folium.plugins import HeatMap
+
 import folium
 from streamlit_folium import folium_static
-import matplotlib.pyplot as plt
-#import flickrapi
-import random
-#from dotenv import load_dotenv
-import os
-import urllib
+
+
+
 import plotly.express as px
 import requests
 import datetime
@@ -25,9 +21,7 @@ st.set_page_config(
 @st.cache
 def load_data():
     """ Load the cleaned data with latitudes, longitudes & timestamps """
-    df = pd.read_csv("station_1.csv")
-    df['id']=pd.to_datetime(df['id'], format='%Y-%m-%d %H:%M:%S', errors='ignore',exact=True)
-    #travel_log["date"] = pd.to_datetime(travel_log["ts"])
+    df= pd.read_csv('bases_bicimad.csv', sep=";")
     return df
 
 
@@ -39,17 +33,11 @@ with tab1:
     st.title("🚲 BiciMad Predictor ")
 
 
-    travel_data = load_data()
-
-
-    stations_data= pd.read_csv('bases_bicimad.csv', sep=";")
+    stations_data= load_data()
     stations_data.rename(columns={'Longitud':'longitude','Latitud':'latitude'},inplace=True)
     serie=stations_data['Direccion'].copy()
     serie.sort_values(axis=0, ascending=True, inplace=True)
 
-    # Calculate the timerange for the slider
-    min_ts = travel_data["id"].min()
-    max_ts = travel_data["id"].max()
 
     now = datetime.datetime.now()
     date=now.date()
@@ -57,22 +45,6 @@ with tab1:
     name_test='Puerta del Sol B'
     options=['--','Address','District','Neighborhood']
 
-
-    #new_row need to be updated by information from the station
-        #number, light, total_bases, longitude, latitude, weather,
-
-    # new_row={'activate':1, 'name':name, 'reservations_count':0, 'light':0, 'total_bases':30,
-    #     'free_bases':28, 'number':'1b', 'longitude':-3.701603, 'no_available':0, 'address':'Puerta del Sol nº 1',
-    #     'latitude':-3.701603, 'dock_bikes':0, 'id':(str(date)+'T'+str(time)), 'time':time, 'date':date, 'holidays':holiday, 'datetime':datetime,
-    #     'feels_like':17.44, 'weather_main':'Rain', 'weekday':weekday, 'year':year, 'month':month, 'hour_sin':hour_sin,
-    #     'hour_cos':hour_cos, 'weekday_sin':weekday_sin, 'weekday_cos':weekday_cos, 'month_sin':month_sin, 'month_cos':month_cos}
-
-    #date_selected= st.sidebar.date_input('When do you want to take your bike', datetime.datetime.today())
-    #time_selected= st.sidebar.time_input('At what time will you take your bike', now)
-    #day_selected =st.sidebar.selectbox('Select day', range(1,31))
-    #month_selected =st.sidebar.selectbox('Select month', ['January','February','March','June','July','August','September','October','November','December'])
-
-    #predict=st.sidebar.text_input('Prueba')
 
 
     url = 'https://bicimad-xk53jytsnq-ew.a.run.app/predict'
@@ -121,25 +93,6 @@ with tab1:
                     st.map( stations_data)
 
 
-
-
-
-    # selected_info=stations_data[stations_data['Direccion']==station_selected]
-    # name=stations_data[stations_data['Direccion']==station_selected]
-
-    # longitude=stations_data.loc[stations_data['Direccion']==station_selected]['longitude'].values[0]
-
-    # latitude=stations_data.loc[stations_data['Direccion']==station_selected]['latitude'].values[0]
-
-    # total_bases =stations_data.loc[stations_data['Direccion']==station_selected]['Número de Plazas'].values[0]
-
-    # number=stations_data.loc[stations_data['Direccion']==station_selected]['Número'].values[0]
-    # number=number.replace(" ", "")
-    # while number=='0':
-    #     number=number[1:]
-    # number_test=number
-    # address=stations_data[stations_data['Direccion']==station_selected]['Direccion']
-    # list_stations=list(stations_data['Número'])
     result=-1
     with col1:
         if st.button('Predict'):
